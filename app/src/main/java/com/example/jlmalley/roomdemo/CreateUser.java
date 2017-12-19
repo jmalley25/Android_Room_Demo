4dd3e078e8aada90ce5dd6d7b63519d2b426e3f7
MALLEY <jlmalley@ndc.nasa.gov>
2017-12-19 11:21:28 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/jlmalley/roomdemo/CreateUser.java
@@ -0,0 +1,18 @@
+package com.example.jlmalley.roomdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jlmalley on 12/19/2017.
+ */
+
+public class CreateUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.create_user );
+    }
+}
