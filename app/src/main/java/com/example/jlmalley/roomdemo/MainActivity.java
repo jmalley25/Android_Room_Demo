7fc2b994bf3524ff0a47daa5b9e8892a05d9cdf7
MALLEY <jlmalley@ndc.nasa.gov>
2017-12-20 14:03:19 -0600

    Set up Recycler View.  Need to find out why the build files keep showing up in the commits.

--- app/src/main/java/com/example/jlmalley/roomdemo/MainActivity.java
+++ app/src/main/java/com/example/jlmalley/roomdemo/MainActivity.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
     FloatingActionButton fab;
+    List<String> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
         Toolbar toolbar = findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
+
+        recyclerView = findViewById( R.id.recycler_view );
+
+        users = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            users.add("Jesse # " + i);
+        }
+
+        recyclerView.setLayoutManager( new LinearLayoutManager( this ) );
+        adapter = new UserAdapter( ( ArrayList<String> ) users );
+        recyclerView.setAdapter( adapter );
+
 
         fab = findViewById( R.id.fab );
         fab.setOnClickListener( new View.OnClickListener() {
