014469b65a414c966efb468b8c6937c5fec26621
MALLEY <jlmalley@ndc.nasa.gov>
2017-12-20 10:43:51 -0600

    Basic input setup completed and functioning properly.

--- app/src/main/java/com/example/jlmalley/roomdemo/CreateUser.java
+++ app/src/main/java/com/example/jlmalley/roomdemo/CreateUser.java
@@ -3,16 +3,37 @@ package com.example.jlmalley.roomdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by jlmalley on 12/19/2017.
- */
 
 public class CreateUser extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    Button button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.create_user );
+        final String TAG = "CreateUser - onCreate";
+
+        firstName = findViewById( R.id.first_name );
+        lastName = findViewById( R.id.last_name );
+        email = findViewById( R.id.email );
+        button = findViewById( R.id.button );
+
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:  Save to database
+                Log.d(TAG, "onClick - firstName: " + firstName.getText().toString());
+            }
+        } );
+
     }
 }
